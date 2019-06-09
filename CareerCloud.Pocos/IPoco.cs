9c43fa046ea021a366af2d6f275b1a240060b0c3
Melvin Sewak <melvinsewak@outlook.com>
2019-06-09 18:31:27 -0400

    Assignment #1 and Assignment #2 setup

---
+++ CareerCloud.Pocos/IPoco.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace CareerCloud.Pocos
+{
+    public interface IPoco
+    {
+        Guid Id { get; set; }
+    }
+}
---
+++ CareerCloud.Pocos/SystemCountryCodePoco.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace CareerCloud.Pocos
+{
+    [Table("System_Country_Codes")]
+    public class SystemCountryCodePoco
+    {
+        [Key]
+        public string Code { get; set; }
+
+        [Column("Name")]
+        public string Name { get; set; }
+    }
+}
